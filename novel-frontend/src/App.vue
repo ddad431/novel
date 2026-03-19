9ac156deed94c8823a4e4189646bb465783608f3
ddad431 <i.lihao@qq.com>
2026-03-19 21:31:21 +0800

    fix: white screen flicker when tab page first load

--- novel-frontend/src/App.vue
+++ novel-frontend/src/App.vue
@@ -124,3 +124,14 @@ onHide(() => {
     console.log('App Hide');
 });
 </script>
+
+<style>
+/* NOTE 在 App.vue 提前应用背景色，防止首次切换到 tab 页时出现白屏闪烁 (原因参见：https://en.uniapp.dcloud.io/tutorial/performance.html#%E4%BC%98%E5%8C%96%E8%83%8C%E6%99%AF%E8%89%B2%E9%97%AA%E7%99%BD) */
+body {
+    background-color: #fff;
+}
+
+body.dark-mode {
+    background-color: #121212;
+}
+</style>
